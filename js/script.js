93c55f748dbb40ac79a5e9e99dcc322ce5a1278d
mena-amer <132224939+mena-amer@users.noreply.github.com>
2023-05-05 16:59:27 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videobtn = document.querySelectorAll('.vid-btn');
+
+window.onscroll = function(){
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+videobtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+
